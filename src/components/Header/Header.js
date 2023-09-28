92f5f77c366c3dcd64b597f879ab347090dc71ea
Diana Sosnovskaya <progdidi@gmail.com>
2023-09-28 17:52:51 +0200

    feat: sticky menu, close menu

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,16 +4,29 @@ import logo from './images/logo.svg';
 import searchBtn from './images/search.svg';
 import cartBtn from './images/cart.svg';
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './header.scss';
 
 const Header = () => {
     const [isNavExpanded, setIsNavExpanded] = useState(false);
     const [isSearchExpanded, setIsSearchExpanded] = useState(false);
 
+    const [menuFixed, setMenuFixed] = useState('header');
+
+    const fixingMenu = () => {
+        if(window !== undefined) {
+            let windowHeight = window.scrollY;
+            windowHeight > 200 ? setMenuFixed('header fixed') : setMenuFixed('header');
+        }
+    }
+    useEffect(() => {
+        window.addEventListener('scroll', fixingMenu);
+    })
+    
+
     return ( 
         <>
-        <header className="header">
+        <header className={menuFixed}>
             <div className="container__fluid">
                 <div className="header__inner">
                     <a href="" className="logo"><img src={logo} alt="" className="logo__img" /></a>
@@ -24,44 +37,44 @@ const Header = () => {
                             <span></span>
                         </button>
                         <ul className={isNavExpanded ? "menu__list menu__list-active" : "menu__list"}>
-                            <li className="menu__list-item">
-                                <NavLink to="/" className="menu__list-link">Home</NavLink>
+                            <li className="menu__list-item" onClick={() => {setIsNavExpanded(false)}}>
+                                <NavLink to="/organik" className="menu__list-link">Home</NavLink>
                             </li>
 
-                            <li className="menu__list-item">
-                                <NavLink to="/aboutus" className="menu__list-link">About</NavLink>
+                            <li className="menu__list-item" onClick={() => {setIsNavExpanded(false)}}>
+                                <NavLink to="/organik/aboutus" className="menu__list-link">About</NavLink>
                             </li>
 
                             <li className="menu__list-item pages">
                                 <a href="" className="menu__list-link">Pages</a>
                                 <ul className="submenu">
                                     <li className="submenu__item">
-                                        <NavLink to="/contacts" className="submenu__link">Contacts</NavLink>
+                                        <NavLink to="/organik/contacts" className="submenu__link">Contacts</NavLink>
                                     </li>
 
                                     <li className="submenu__item">
-                                        <NavLink to="/services" className="submenu__link">Services</NavLink>
+                                        <NavLink to="/organik/services" className="submenu__link">Services</NavLink>
                                     </li>
                                     <li className="submenu__item">
-                                        <NavLink to="/standart" className="submenu__link">Standarts</NavLink>
+                                        <NavLink to="/organik/standart" className="submenu__link">Standarts</NavLink>
                                     </li>
 
                                     <li className="submenu__item">
-                                        <NavLink to="/license" className="submenu__link">Licences</NavLink>
+                                        <NavLink to="/organik/license" className="submenu__link">Licences</NavLink>
                                     </li>
                                 </ul>
                             </li>
 
-                            <li className="menu__list-item">
-                                <NavLink to="/shop" className="menu__list-link">Shop</NavLink>
+                            <li className="menu__list-item" onClick={() => {setIsNavExpanded(false)}}>
+                                <NavLink to="/organik/shop" className="menu__list-link">Shop</NavLink>
                             </li>
 
-                            <li className="menu__list-item">
+                            <li className="menu__list-item" onClick={() => {setIsNavExpanded(false)}}>
                                 <a href="" className="menu__list-link">Projects</a>
                             </li>
 
-                            <li className="menu__list-item">
-                                <NavLink to="/news" className="menu__list-link">News</NavLink>
+                            <li className="menu__list-item" onClick={() => {setIsNavExpanded(false)}}>
+                                <NavLink to="/organik/news" className="menu__list-link">News</NavLink>
                             </li>
 
                         </ul>
